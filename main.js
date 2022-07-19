545e8845f01907557f24352b3e61f9fc61263fe4
Alexandru Popescu <alexandrupopescu365@gmail.com>
2022-07-19 15:37:15 +0300

    finish

--- main.js
+++ main.js
@@ -1,46 +1,17 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart:0,
-            product: 'Socks',
-            brand: 'Adibas',
-            selectedVariant: 0,
-            details: ['50% cotton', '30% wool', '20% polyester'],
-            variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
-            ],
-            onSale: true
+          cart: [],
+          premium: false
         }
-    },
-    methods: {
-        addToCart() {
-            this.cart += 1
+      },
+      methods: {
+        updateCart(id) {
+            this.cart.push(id)
         },
-        removeFromCart() {
-            if(this.cart > 0) { 
-                this.cart -= 1 
-            }
+        updateCart2(id) {
+            this.cart.shift(id)
         },
-        updateVariant(index) {
-            this.selectedVariant = index
-        }
-    },
-    computed: {
-        title() {
-            return this.brand + ' ' + this.product
-        },
-        image() {
-            return this.variants[this.selectedVariant].image
-        },
-        inStock() {
-            return this.variants[this.selectedVariant].quantity
-        },
-        saleMessage() {
-            if (this.onSale) {
-                return this.brand + ' ' + this.product + ' is on sale.'
-            }
-            return ''
-        }
-    }
-})
+        
+      }
+    })
